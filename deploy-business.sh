998a8880fb080449ac815e82b788ae5e843d9403
manuelinco <piscitellimanuel5@gmail.com>
2026-05-14 11:24:23 +0200

    fix: export CLOUDFLARE_API_TOKEN from CF_PAGES_TOKEN with debug

--- deploy-business.sh
+++ deploy-business.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-CLOUDFLARE_API_TOKEN="$CF_PAGES_TOKEN" npx wrangler pages deploy dist-business
+echo "CF_PAGES_TOKEN set: ${#CF_PAGES_TOKEN} chars"
+export CLOUDFLARE_API_TOKEN="$CF_PAGES_TOKEN"
+echo "CLOUDFLARE_API_TOKEN set: ${#CLOUDFLARE_API_TOKEN} chars"
+npx wrangler pages deploy dist-business
